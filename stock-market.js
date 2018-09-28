0651d65af06e6fb9897996f7efb71e9aee0db028
Zachary Lee <zacharylhlee@gmail.com>
2018-09-28 18:39:44 +0000

    Add elegant solution

--- stock-market.js
+++ stock-market.js
@@ -1,5 +1,6 @@
 var input = [45, 24, 35, 31, 40, 38, 11];
 
+//Brute force solution: Check every possibility
 function maxProfit(arr) {
   var result = -1
   for (var i = 0; i < input.length; i++) {
@@ -15,4 +16,20 @@ function maxProfit(arr) {
   return result;
 }
 
-console.log(maxProfit(input));
+//Elegant solution: Only iterate through the array once
+function elegantMaxProfit(arr) {
+  var maxProfit = 0;
+  var minBuy = arr[0];
+  arr.forEach(function(element) {
+    if (element < minBuy) {
+      minBuy = element;
+    }
+    profit = element - minBuy;
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+  });
+  return maxProfit;
+}
+
+console.log(elegantMaxProfit(input));
