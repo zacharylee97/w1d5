d4c3e09ba0bd27f1181cdcc91629ef24332ec86e
Zachary Lee <zacharylhlee@gmail.com>
2018-09-28 15:18:09 +0000

    Fixed palindromes.js and verified tests

--- test-store-list.js
+++ test-store-list.js
@@ -1,9 +1,9 @@
 var storeList = require("./store-list");
 
-storeList.addToList("5");
-storeList.addToList("50");
-storeList.addToList("3");
-storeList.addToList("17");
-storeList.addToList("9");
+storeList.addToList(5);
+storeList.addToList(50);
+storeList.addToList(3);
+storeList.addToList(17);
+storeList.addToList(9);
 
 console.log(storeList.sortedList());
